5fa01026feb364f3b1fc3b56d9543a098927a5ac
Keith Charles <keithc@ouraysportswear.com>
2020-09-15 11:36:29 -0600

    Updates

---
+++ precache-manifest.3224f844eb8bbe174ed989d434db898c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcb405da0b7ac6fd591e143fb22ee1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0001f542a2945befd073",
+    "url": "/static/css/main.e61a2245.chunk.css"
+  },
+  {
+    "revision": "f1c4cd464081fa7a51fe",
+    "url": "/static/js/2.600fb0df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.600fb0df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0001f542a2945befd073",
+    "url": "/static/js/main.bac41094.chunk.js"
+  },
+  {
+    "revision": "c5c5d11a5123bae317ed",
+    "url": "/static/js/runtime-main.9935317f.js"
+  },
+  {
+    "revision": "f2328b81b9fd274728289a9a17a8a021",
+    "url": "/static/media/logo-img.f2328b81.png"
+  }
+]);
