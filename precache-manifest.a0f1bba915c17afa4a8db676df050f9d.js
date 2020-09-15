454a4fd64c1521ce5dfa1500685e955ff4e19b8b
Keith Charles <keithc@ouraysportswear.com>
2020-09-15 11:47:03 -0600

    Updates

---
+++ precache-manifest.a0f1bba915c17afa4a8db676df050f9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209b411771dda90fbaf09f6a855e46b8",
+    "url": "/InVisionAR-frontend/index.html"
+  },
+  {
+    "revision": "59bb723daf87702bf5e1",
+    "url": "/InVisionAR-frontend/static/css/main.e61a2245.chunk.css"
+  },
+  {
+    "revision": "f1c4cd464081fa7a51fe",
+    "url": "/InVisionAR-frontend/static/js/2.600fb0df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/InVisionAR-frontend/static/js/2.600fb0df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bb723daf87702bf5e1",
+    "url": "/InVisionAR-frontend/static/js/main.cef23664.chunk.js"
+  },
+  {
+    "revision": "f157cc7016c59264a13f",
+    "url": "/InVisionAR-frontend/static/js/runtime-main.2c92c551.js"
+  },
+  {
+    "revision": "f2328b81b9fd274728289a9a17a8a021",
+    "url": "/InVisionAR-frontend/static/media/logo-img.f2328b81.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3224f844eb8bbe174ed989d434db898c.js"
+  "/InVisionAR-frontend/precache-manifest.a0f1bba915c17afa4a8db676df050f9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/InVisionAR-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
